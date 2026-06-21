f34b7c3ec3aa59cfe20b0eeadecbf483c303a7af
sunxiao5 <sunxiao5@staff.sina.com.cn>
2026-06-21 16:20:40 +0800

    For now, all cross-domain restrictions will be enabled. This will need to be addressed during the official release.

--- server/middleware/cors_middleware.py
+++ server/middleware/cors_middleware.py
@@ -8,10 +8,12 @@ def register_cors_middleware(app: FastAPI):
         CORSMiddleware,
         # 允许跨域的源地址
         allow_origins=[
-            "http://localhost:8080",
-            "http://127.0.0.1:8080",
+            # "http://localhost:8080",
+            # "http://localhost:5173",
+            # "http://127.0.0.1:8080",
+            # "http://127.0.0.1:5173",
             # "*" 表示允许所有域名(开发临时用，生产不推荐)
-            # "*"
+            "*"
         ],
         allow_credentials=True,  # 允许携带 Cookie
         allow_methods=["*"],  # 允许所有请求方法: GET/POST/PUT/DELETE...
