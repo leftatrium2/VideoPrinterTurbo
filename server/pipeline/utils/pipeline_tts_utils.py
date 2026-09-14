f19d9e36242f7a1cf43017bcab36dc7ba29766ee
sunxiao5 <sunxiao5@staff.sina.com.cn>
2026-09-14 09:41:29 +0800

    test for tts utils function
    test for tts utils function

--- server/pipeline/utils/pipeline_tts_utils.py
+++ server/pipeline/utils/pipeline_tts_utils.py
@@ -1,3 +1,7 @@
+import asyncio
+import os
+from typing import Optional
+
 import config.config as _config
 from pipeline.tts.azure_tts_v1 import AzureTTSV1
 from pipeline.tts.azure_tts_v2 import AzureTTSV2
@@ -6,26 +10,71 @@ from pipeline.tts.google_gemini_tts import GoogleGeminiTTS
 from pipeline.tts.siliconflow_tts import SiliconFlowTTS
 from pipeline.utils.pipeline_video_downloader_utils import init_downloader
 from utils import const
+from utils.exception import VPTException
+from utils.file_utils import get_llm_rewrite_path
 
 
-def tts(tts_engine: str, subtitle_path: str, lang: str, voice: str, api_key: str = None,
-        region: str = None, proxy: str = None) -> bool:
-    tts: TTSBase = None
+def tts_from_subtitle(
+        tts_engine: str,
+        subtitle_path: str,
+        voice: str,
+        lang: str,
+        api_key: Optional[str] = None,
+        region: Optional[str] = None,
+        proxy_type: int = const.PROXY_CONFIG_TYPE_UNKNOWN,
+        proxy_url: Optional[str] = None
+
+) -> bool:
+    tts_service: Optional[TTSBase] = None
     if tts_engine == const.TTS_LIST_AZURE_TTS_V1_VAL:
-        tts = AzureTTSV1()
+        tts_service = AzureTTSV1(proxy_type=proxy_type, proxy_url=proxy_url)
     elif tts_engine == const.TTS_LIST_AZURE_TTS_V2_VAL:
-        tts = AzureTTSV2()
+        if not api_key:
+            raise VPTException(const.PIPELINE_ERR_VALUE, "api key is empty!")
+        if not region:
+            raise VPTException(const.PIPELINE_ERR_VALUE, "region is empty!")
+        tts_service = AzureTTSV2(
+            api_key=api_key,
+            region=region,
+            proxy_type=proxy_type,
+            proxy_url=proxy_url
+        )
     elif tts_engine == const.TTS_LIST_SILICON_FLOW_TTS_VAL:
-        tts = SiliconFlowTTS()
+        if not api_key:
+            raise VPTException(const.PIPELINE_ERR_VALUE, "api key is empty!")
+        tts_service = SiliconFlowTTS(
+            api_key=api_key,
+            proxy_type=proxy_type,
+            proxy_url=proxy_url
+        )
     elif tts_engine == const.TTS_LIST_GOOGLE_GEMINI_TTS_VAL:
-        tts = GoogleGeminiTTS()
-    if not tts:
+        if not api_key:
+            raise VPTException(const.PIPELINE_ERR_VALUE, "api key is empty!")
+        tts_service = GoogleGeminiTTS(
+            api_key=api_key,
+            proxy_type=proxy_type,
+            proxy_url=proxy_url
+        )
+    if not tts_service:
         return False
-    tts.config(api_key=api_key, region=region, proxy=proxy)
-    tts.rewrite(subtitle_path, lang, voice)
+    tts_service.rewrite(subtitle_path, lang, voice)
     return True
 
 
 if __name__ == "__main__":
     _config.init_config()
     init_downloader()
+
+    lang = asyncio.run(get_llm_rewrite_path())
+    llm_rewrite_path = os.path.join(lang, "20260720215545133997.srt")
+
+    tts_from_subtitle(
+        tts_engine="TTS_LIST_AZURE_TTS_V2",
+        subtitle_path=llm_rewrite_path,
+        voice="zh-CN-XiaoxiaoNeural",
+        lang="zh-CN",
+        api_key="",
+        region="koreacentral",
+        proxy_type=const.PROXY_CONFIG_TYPE_HTTPS,
+        proxy_url="http://127.0.0.1:7890"
+    )
